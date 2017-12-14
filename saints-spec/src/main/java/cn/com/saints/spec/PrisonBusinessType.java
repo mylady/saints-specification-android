99e8eb889f4f954cba88ccd29b751905c31a6467
mylady <closezling@outlook.com>
2017-12-14 15:35:31 +0800

    add new definition

--- saints-spec/src/main/java/cn/com/saints/spec/PrisonBusinessType.java
+++ saints-spec/src/main/java/cn/com/saints/spec/PrisonBusinessType.java
@@ -8,6 +8,8 @@ public class PrisonBusinessType {
     public static final int MeetingArraignRecord = 0;
     public static final int CellTerminalNotify = 1;
     public static final int MeetingArraignFeedback = 2;
-    public static final int FaceRollCallStart = 3;
+    public static final int StartFaceRollCall = 3;
     public static final int FaceRollCallFeedback = 4;
+    public static final int FaceRollCallCellEnd = 5;
+    public static final int StopFaceRollCall = 6;
 }
---
+++ saints-spec/src/main/java/cn/com/saints/spec/PrisonTerminalType.java
@@ -0,0 +1,11 @@
+package cn.com.saints.spec;
+
+/**
+ * Created by MyLady on 14/12/2017.
+ */
+
+public class PrisonTerminalType {
+    public static int OutsideTerminal = 0;
+    public static int InsideTerminal = 1;
+}
+
